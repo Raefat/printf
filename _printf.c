ba7845219e294055c99f595d6f57402642b23c5a
Raefetoxic <raefetex@gmail.com>
2023-08-18 09:43:19 -0700

    task 0 - printf project

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - that produces output according to a format.
+ *
+ * @format: the format that needs to be procceced
+ *
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIH_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+
+#endif
